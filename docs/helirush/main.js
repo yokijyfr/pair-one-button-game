608f5554f71fbc88d53549f5cff567adba7e80e5
yokijyfr <103284162+yokijyfr@users.noreply.github.com>
2022-10-12 00:18:34 -0700

    123123

--- docs/helirush/main.js
+++ docs/helirush/main.js
@@ -1,243 +1,153 @@
-title = "helirush";
+title = "bubbleshot";
 
 description = `
 [click] shot
 `;
-		characters = [
-			`
- GGGGGG
-g   g
-ggggggg
-g  gggg
-`,
-			`
- GGGGGGG
-g   g
-ggggggg
-g  gggg
-  `,
-			`
-GGGGGGG
-g   g
-ggggggg
-g  gggg
-  `,
-		];
+characters = [];
 
-		options = {
-			theme: "dark",
-			// isPlayingBgm: true,
-			// isReplayEnabled: true,
-			seed: 8000,
-			viewSize: { x: 100, y: 120 }
-		};
+options = {
+  isDrawingScoreFront: true,
+  seed: 9,
+};
 
-		/**
-		 * @type {{
-		 * from: Vector, to: Vector, vel: Vector,
-		 * ticks: number, prevLine: any, isActive: boolean
-		 * }[]}
-		 */
-		let lines;
-		let activeTicks;
-		/** @type {{pos: Vector, vel: Vector}[]} */
-		let human;
+/** @type {{x: number, y: number, vx: number, bars: {width: number, color: Color}[]}[]} */
+let lanes;
+let laneY;
+let shotY;
+let hitColor;
+let laneCount;
+let baseMultiplier;
+let multiplier;
+let penalty;
+const colors = ["red", "green", "blue", "yellow"];
+const laneHeight = 7;
 
+function update() {
+  if (!ticks) {
+    lanes = [];
+    shotY = undefined;
+    hitColor = undefined;
+    laneCount = 2;
+    times(laneCount, () => addLane());
+    baseMultiplier = 0;
+    multiplier = 1;
+    penalty = 1;
+  }
+  let sy = 97;
+  if (shotY != null) {
+    shotY -= sqrt(difficulty) * 3;
+    sy = shotY;
+  } else {
+    hitColor = undefined;
+    if (input.isJustPressed) {
+      play("laser");
+      multiplier = 1;
+      shotY = sy;
+      laneY += 2 * penalty * sqrt(sqrt(difficulty));
+    }
+  }
+  color(hitColor == null ? "black" : hitColor);
+  rect(49, sy, 3, 99 - sy);
+  const my = laneHeight * laneCount;
+  laneY += sqrt(difficulty) * 0.005;
+  if (laneY < my) {
+    laneY += (my - laneY) * 0.2;
+  }
+  let ly = laneY;
+  remove(lanes, (l) => {
+    l.x = wrap(l.x + l.vx, 0, 99);
+    l.y += (ly - l.y) * 0.2;
+    let x = l.x;
+    let isRemoved = false;
+    let isShotRemoved = false;
+    l.bars.forEach((b) => {
+      color(b.color);
+      let c;
+      if (x + b.width < 99) {
+        c = rect(x, l.y, b.width - 1, -laneHeight + 1).isColliding.rect;
+      } else {
+        c = rect(x, l.y, 99 - x, -laneHeight + 1).isColliding.rect;
+        c = {
+          ...c,
+          ...rect(0, l.y, b.width - (99 - x) - 1, -laneHeight + 1).isColliding
+            .rect,
+        };
+      }
+      if (c.black) {
+        hitColor = b.color;
+        isRemoved = true;
+      } else if (hitColor != null) {
+        if (c[b.color]) {
+          isRemoved = true;
+        } else if (c.red || c.green || c.blue || c.yellow) {
+          isShotRemoved = true;
+        }
+      }
+      x = wrap(x + b.width, 0, 99);
+    });
+    ly -= laneHeight;
+    if (isShotRemoved) {
+      play("hit");
+      shotY = undefined;
+      penalty = clamp(penalty * (3 / multiplier), 1, 4);
+    } else if (isRemoved) {
+      addScore(multiplier * pow(2, baseMultiplier), 50, l.y);
+      laneY -= multiplier;
+      multiplier *= 2;
+      return true;
+    }
+  });
+  if (lanes.length === 0) {
+    shotY = undefined;
+    laneCount++;
+    if (laneCount > clamp(5 + baseMultiplier, 1, 10)) {
+      baseMultiplier = clamp(baseMultiplier + 1, 1, 9);
+      laneCount = 2;
+    }
+  }
+  if (shotY == null) {
+    times(laneCount - lanes.length, () => addLane());
+    if (shotY == null && lanes[0].y > 97) {
+      play("explosion");
+      end();
+    }
+  }
 
+  function addLane() {
+    play("select");
+    const x = rnd(99);
+    const vx = rnds(0.5, 1) * sqrt(difficulty);
+    if (lanes.length === 0) {
+      laneY = 0;
+      lanes.push({ x, y: 0, vx, bars: addBars() });
+    } else {
+      lanes.push({
+        x,
+        y: -lanes.length * laneHeight,
+        vx,
+        bars: addBars(lanes[lanes.length - 1].bars),
+      });
+    }
+  }
 
-		/**
-		 * @typedef { object } player
-		 * @property { Vector } pos
-		 * @property { number } firingrate
-		 * @property { number } moving_velocity
-		 */
-		let player;
-
-		/**
-			* @typedef { object } enemies
-			* @property { Vector } pos
-		 */
-		let enemies;
-		let number_enemies;
-
-		// declare Bullet
-		/**
-		 * @typedef { object } Bullet
-		 * @property { Vector } pos
-		 * @property { Vector } moving_velocity
-		 */
-		let Bullet;
-		let bullet_speed;
-
-
-		let firstClick;
-		let secondClick;
-		let twoClicksDuration;
-		let holdDuration;
-
-		let fireTrigger;
-
-		let game_score;
-
-		function update() {
-			if (!ticks) {
-				lines = [];
-				activeTicks = -1;
-				human = [];
-
-				//init player
-				player = {
-					pos: vec(50, 30),
-					firingrate: 50,
-					moving_velocity: 1,
-				};
-
-				//init bullet
-				bullet_speed = 1;
-
-				//init enemies
-				enemies = [];
-				number_enemies = 5;
-
-				//setup mouse input
-				firstClick = false;
-				secondClick = false;
-				twoClicksDuration = 0;
-				holdDuration = 0;
-				//fire setup
-				fireTrigger = false;
-
-				//game score
-				game_score = 30;
-			}
-
-
-
-			//   if (lines.length === 0) {
-			//     const from = vec(rnd(30, 70), 0);
-			//     lines.push({
-			//       from,
-			//       to: vec(from),
-			//       vel: vec(0.5 * difficulty, 0).rotate(PI / 2),
-			//       ticks: ceil(30 / difficulty),
-			//       prevLine: undefined,
-			//       isActive: false,
-			//     });
-			//   }
-
-			if ((player.pos.x < 0) || (player.pos.x > 99)) {
-				//  play("laser");
-				player.moving_velocity *= -1;
-			}
-
-
-
-			//draw player
-			player.pos.x += player.moving_velocity;
-			color("black");
-			char("a", player.pos);
-
-
-			if (input.isJustPressed) {
-				//first click = true means it's single click
-				if (!firstClick) {
-					firstClick = true;
-				} else {
-					//second click = true means it's double click
-					secondClick = true;
-					firstClick = false;
-				}
-			}
-
-			if (input.isPressed) {
-				holdDuration++;
-				//if u hold, it will run hold code instead of single/double click
-				if (holdDuration >= 15) {
-					//=====================================
-					//put your code for holding here
-					console.log("holding");
-					//=====================================
-					secondClick = false;
-					firstClick = false;
-				}
-			} else {
-				if (firstClick && twoClicksDuration < 7) {
-					twoClicksDuration++;
-				} else {
-					if (firstClick && holdDuration < 15) {
-						//=====================================
-						//put your code for single click here
-						console.log("just a click");
-
-						if (!fireTrigger) {
-
-							fireTrigger = true;
-							Bullet = {
-								pos: vec(player.pos),
-								moving_velocity: bullet_speed,
-							}
-
-
-						}
-
-
-						//=====================================
-					}
-					if (secondClick && holdDuration < 15) {
-						//=====================================
-						//put your code for double click here
-						console.log("double click");
-						//=====================================
-						secondClick = false;
-					}
-					holdDuration = 0;
-					twoClicksDuration = 0;
-					firstClick = false;
-				}
-
-			}
-
-			if (fireTrigger) {
-				color("yellow");
-				box(Bullet.pos, 2, 3);
-				Bullet.pos.y += Bullet.moving_velocity;
-
-				if (Bullet.pos.y >= 120) {
-					fireTrigger = false;
-				}
-			}
-
-
-			if (enemies.length === 0) {
-
-				for (let i = 0; i < number_enemies; i++) {
-					const posX = rnd(20, 100);
-					const posY = 100;
-					enemies.push({
-						pos: vec(posX, posY)
-					})
-				}
-			}
-
-
-
-			// This time, with remove()
-			remove(enemies, (e) => {
-
-				color("red");
-				const isCollidingWithBullets = box(e.pos, 10, 10).isColliding.rect.yellow;
-				if (isCollidingWithBullets) {
-					play("explosion");
-					color("yellow");
-					particle(e.pos,
-						100, // The number of particles
-						1, // The speed of the particles
-						-PI / 2, // The emitting angle
-						PI  // The emitting width
-					);
-					game_score += 10;
-					addScore(game_score, e.pos);
-				}
-				return (isCollidingWithBullets);
-			});
-				}
+  function addBars(prevBars) {
+    let cs =
+      prevBars != null
+        ? prevBars.map((b) => b.color)
+        : [colors[rndi(colors.length)]];
+    if (cs.length === 1 || (cs.length < 4 && rnd() < 0.5)) {
+      cs.push(colors[rndi(colors.length)]);
+    } else {
+      cs.splice(rndi(colors.length), 1);
+    }
+    let lx = 99;
+    let x = rnd(99);
+    const cc = cs.length;
+    return cs.map((c, i) => {
+      const width = i === cc - 1 ? lx : (99 / cc) * rnd(0.8, 1.2);
+      lx -= width;
+      x = wrap(x + width, 0, 99);
+      return { x, width, color: c };
+    });
+  }
+}
